0de09f6893b4aa23eeaed43339f2d7d6dc5ce7b7
Akrati Pandey <211220004@nitdelhi.ac.in>
2023-06-23 11:04:13 +0530

    my intro

---
+++ index.js
@@ -0,0 +1,34 @@
+/* 
+   --> git init -- To initialize
+   --> ls -a -- for seeing all files including hidden
+   --> git status -- to check status of files if they're in staging area or not 
+   --> git add fileName.extension -- To add files in staging area
+   --> git add . -- to specify all files inside current dir
+   --> git commit -m "commitMessage" -- Commit msg help you identify what changes uh commit to file
+
+
+   --> git log -- To see details of what commit you have made
+   --> git diff chap2.txt -- To see diff btw updated n prev committed version of a file
+   --> git checkout fileName.extension -- It'll roll back you to last committed file n erase the new un-committed file
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
